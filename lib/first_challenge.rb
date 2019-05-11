49a4956483ed6393a11765d73052c2def51a490c
Ben Buchmann <ben_buchy92@hotmail.com>
2019-05-11 18:47:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,4 +1,15 @@
-def Epic_tragedy = { :Montague => {}
-  :Capulet => {}
+def Epic_tragedy = { :Montague => {
+  :Patriarch => {},
+  :Matriarch => {},
+  :Hero => {},
+  :Hero_Friends => []
+},
+  :Capulet => {
+    :Patriarch => {}
+    :Matriarch => {}
+    :Heroine => {}
+    :Heroine_friends => {}
+    []
+    }
 }
 end
